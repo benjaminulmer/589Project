024694c0e32fbed0bbf3967389ba9862abdfe1d8
jshall <jshall@gfx02-nc.cs.ucalgary.ca>
2017-03-24 18:58:17 -0600

    Code for contacts fleshed out, not yet running, still need debugging

--- src/ModelSplitter.h
+++ src/ModelSplitter.h
@@ -8,10 +8,14 @@
 #include "Renderable.h"
 
 class Renderable;
+struct Block;
 
 class ModelSplitter {
 
 public:
 	std::vector<Renderable*> split(Renderable* object);
+	std::vector<Block*> contactsAndBlocking(std::vector<Renderable*> objects);
+	bool lineIntersect(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3, glm::vec3 v4);
+	bool pointInTriangle(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3, glm::vec3 p);
 
 };
